eed50dfff21ddd2341aaa2a7a5f64a5744cfab96
Nicolas Miguel <nicolasmiguelsantossilva@gmail.com>
2025-05-19 15:07:42 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn23 = new System.Windows.Forms.Button();
+            this.btn31 = new System.Windows.Forms.Button();
+            this.btn32 = new System.Windows.Forms.Button();
+            this.btn33 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pibFechar = new System.Windows.Forms.PictureBox();
+            this.pibReiniciar = new System.Windows.Forms.PictureBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibFechar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibReiniciar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn11
+            // 
+            this.btn11.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn11.Location = new System.Drawing.Point(61, 146);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(89, 70);
+            this.btn11.TabIndex = 0;
+            this.btn11.UseVisualStyleBackColor = true;
+            // 
+            // btn12
+            // 
+            this.btn12.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn12.Location = new System.Drawing.Point(176, 146);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(89, 70);
+            this.btn12.TabIndex = 1;
+            this.btn12.UseVisualStyleBackColor = true;
+            // 
+            // btn13
+            // 
+            this.btn13.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn13.Location = new System.Drawing.Point(287, 146);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(89, 70);
+            this.btn13.TabIndex = 2;
+            this.btn13.UseVisualStyleBackColor = true;
+            // 
+            // btn21
+            // 
+            this.btn21.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn21.Location = new System.Drawing.Point(64, 256);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(89, 70);
+            this.btn21.TabIndex = 3;
+            this.btn21.UseVisualStyleBackColor = true;
+            // 
+            // btn22
+            // 
+            this.btn22.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn22.Location = new System.Drawing.Point(176, 255);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(89, 70);
+            this.btn22.TabIndex = 4;
+            this.btn22.UseVisualStyleBackColor = true;
+            // 
+            // btn23
+            // 
+            this.btn23.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn23.Location = new System.Drawing.Point(287, 256);
+            this.btn23.Name = "btn23";
+            this.btn23.Size = new System.Drawing.Size(89, 70);
+            this.btn23.TabIndex = 5;
+            this.btn23.UseVisualStyleBackColor = true;
+            // 
+            // btn31
+            // 
+            this.btn31.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn31.Location = new System.Drawing.Point(63, 355);
+            this.btn31.Name = "btn31";
+            this.btn31.Size = new System.Drawing.Size(89, 70);
+            this.btn31.TabIndex = 6;
+            this.btn31.UseVisualStyleBackColor = true;
+            // 
+            // btn32
+            // 
+            this.btn32.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn32.Location = new System.Drawing.Point(176, 355);
+            this.btn32.Name = "btn32";
+            this.btn32.Size = new System.Drawing.Size(89, 70);
+            this.btn32.TabIndex = 7;
+            this.btn32.UseVisualStyleBackColor = true;
+            // 
+            // btn33
+            // 
+            this.btn33.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn33.Location = new System.Drawing.Point(287, 356);
+            this.btn33.Name = "btn33";
+            this.btn33.Size = new System.Drawing.Size(89, 70);
+            this.btn33.TabIndex = 8;
+            this.btn33.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(48, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(27, 100);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(390, 365);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pibFechar
+            // 
+            this.pibFechar.Image = ((System.Drawing.Image)(resources.GetObject("pibFechar.Image")));
+            this.pibFechar.Location = new System.Drawing.Point(372, 12);
+            this.pibFechar.Name = "pibFechar";
+            this.pibFechar.Size = new System.Drawing.Size(45, 41);
+            this.pibFechar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibFechar.TabIndex = 11;
+            this.pibFechar.TabStop = false;
+            this.pibFechar.Click += new System.EventHandler(this.pibFechar_Click);
+            // 
+            // pibReiniciar
+            // 
+            this.pibReiniciar.Image = ((System.Drawing.Image)(resources.GetObject("pibReiniciar.Image")));
+            this.pibReiniciar.Location = new System.Drawing.Point(353, 472);
+            this.pibReiniciar.Name = "pibReiniciar";
+            this.pibReiniciar.Size = new System.Drawing.Size(69, 50);
+            this.pibReiniciar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibReiniciar.TabIndex = 12;
+            this.pibReiniciar.TabStop = false;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(28, 479);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(219, 39);
+            this.lblInfo.TabIndex = 13;
+            this.lblInfo.Text = "é a vez do X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 546);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.pibReiniciar);
+            this.Controls.Add(this.pibFechar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn33);
+            this.Controls.Add(this.btn32);
+            this.Controls.Add(this.btn31);
+            this.Controls.Add(this.btn23);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn13);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da velha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibFechar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibReiniciar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn23;
+        private System.Windows.Forms.Button btn31;
+        private System.Windows.Forms.Button btn32;
+        private System.Windows.Forms.Button btn33;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pibFechar;
+        private System.Windows.Forms.PictureBox pibReiniciar;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
